4a9ea9f1a5d2fd671fea535d61981ec9e64403f1
frankelau <liuzhao1@sina.cn>
2016-12-08 18:45:07 +0800

    增加日志支持和jdbcTemplate操作数据库

--- src/main/java/cn/com/liu/controller/HelloController.java
+++ src/main/java/cn/com/liu/controller/HelloController.java
@@ -1,5 +1,6 @@
 package cn.com.liu.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-
+    private static Logger logger = Logger.getLogger(HelloController.class);
 
     @RequestMapping("hello")
     public String index(){
+        logger.info("开始进入HelloWorld控制器...");
         return "Hello World";
     }
 
